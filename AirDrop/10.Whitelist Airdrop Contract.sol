95d80185739353d10011323a1998f35b60707efe
Syed Sameer Shah <78991984+sscodez@users.noreply.github.com>
2023-04-18 23:54:35 +0500

    Update 10.Whitelist Airdrop Contract.sol

--- AirDrop/10.Whitelist Airdrop Contract.sol
+++ AirDrop/10.Whitelist Airdrop Contract.sol
@@ -1,1 +1,36 @@
+pragma solidity ^0.8.0;
+
+interface IERC20 {
+    function transfer(address to, uint256 amount) external returns (bool);
+    function balanceOf(address account) external view returns (uint256);
+}
+
+contract WhitelistAirdrop {
+    address public tokenAddress;
+    uint256 public tokenAmount;
+
+    mapping(address => bool) public whitelist;
+
+    constructor(address _tokenAddress, uint256 _tokenAmount) {
+        tokenAddress = _tokenAddress;
+        tokenAmount = _tokenAmount;
+    }
+
+    function addToWhitelist(address _user) public {
+        whitelist[_user] = true;
+    }
+
+    function removeFromWhitelist(address _user) public {
+        whitelist[_user] = false;
+    }
+
+    function airdrop() public {
+        IERC20 token = IERC20(tokenAddress);
+
+        require(whitelist[msg.sender], "You are not whitelisted");
+        require(token.balanceOf(address(this)) >= tokenAmount, "Insufficient balance of token");
+
+        token.transfer(msg.sender, tokenAmount);
+    }
+}
 

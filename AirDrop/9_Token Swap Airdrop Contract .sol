565e6eb17539b0a40c176e1f6e887ab74e3dd456
Syed Sameer Shah <78991984+sscodez@users.noreply.github.com>
2023-04-18 23:58:05 +0500

    Update 9_Token Swap Airdrop Contract .sol

--- AirDrop/9_Token Swap Airdrop Contract .sol
+++ AirDrop/9_Token Swap Airdrop Contract .sol
@@ -1,32 +1,32 @@
 pragma solidity ^0.8.0;
 
 interface IERC20 {
-    function transfer(address to, uint256 amount) external returns (bool);
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
     function balanceOf(address account) external view returns (uint256);
 }
 
 contract TokenSwapAirdrop {
-    address public token1Address;
-    address public token2Address;
-    uint256 public token1Amount;
-    uint256 public token2Amount;
-
-    constructor(address _token1Address, address _token2Address, uint256 _token1Amount, uint256 _token2Amount) {
-        token1Address = _token1Address;
-        token2Address = _token2Address;
-        token1Amount = _token1Amount;
-        token2Amount = _token2Amount;
+    address public tokenAddress;
+    uint256 public airdropAmount;
+    uint256 public totalAirdropAmount;
+    mapping(address => bool) public claimed;
+    
+    constructor(address _tokenAddress, uint256 _airdropAmount) {
+        tokenAddress = _tokenAddress;
+        airdropAmount = _airdropAmount;
     }
-
-    function swap() public {
-        IERC20 token1 = IERC20(token1Address);
-        IERC20 token2 = IERC20(token2Address);
-
-        require(token1.balanceOf(msg.sender) >= token1Amount, "Insufficient balance of token1");
-        require(token2.balanceOf(address(this)) >= token2Amount, "Insufficient balance of token2");
-
-        token1.transferFrom(msg.sender, address(this), token1Amount);
-        token2.transfer(msg.sender, token2Amount);
+    
+    function swapTokens() external {
+        require(!claimed[msg.sender], "Already claimed");
+        
+        uint256 balance = IERC20(tokenAddress).balanceOf(msg.sender);
+        require(balance > 0, "No tokens to swap");
+        
+        require(IERC20(tokenAddress).transferFrom(msg.sender, address(this), balance), "Transfer failed");
+        
+        claimed[msg.sender] = true;
+        totalAirdropAmount += airdropAmount;
+        
+        require(IERC20(tokenAddress).transfer(msg.sender, airdropAmount), "Airdrop failed");
     }
 }
-
